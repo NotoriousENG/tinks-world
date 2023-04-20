ff41ea31b66ee9d65d7d372d7bcbf85500418ff4
Michael O'Connell <oconnellmj16@gmail.com>
2023-04-19 23:57:48 -0400

    emscripten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import colorsys
+import asyncio
 import pygame
+import colorsys
+import sys
 
 from scene import Scene
 
@@ -9,43 +11,48 @@ def hsv_shift(r,g,b, delta):
     r,g,b = colorsys.hsv_to_rgb(h,s,v)
     return (r,g,b)
 
-pygame.init()
+async def main():
+    pygame.init()
+    pygame.mixer.init()
 
-display_width = 800
-display_height = 600
+    display_width = 800
+    display_height = 600
 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption("Tink's World")
+    gameDisplay = pygame.display.set_mode([display_width,display_height])
+    pygame.display.set_caption("Tink's World")
 
-current_scene = Scene(display_width, display_height)
-current_scene.begin()
+    current_scene = Scene(display_width, display_height)
+    current_scene.begin()
 
-clear_color = (125, 255, 134)
+    clear_color = (125, 255, 134)
 
-clock = pygame.time.Clock()
-crashed = False
+    clock = pygame.time.Clock()
+    crashed = False
 
-music = pygame.mixer_music.load('cat_song.wav')
+    pygame.mixer.music.load('cat_song.ogg', 'ogg')
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.2)
 
-pygame.mixer_music.play(-1)
+    # Game Loop
+    while not crashed:
+        delta = float(clock.get_time()) / 1000.0
 
-# Game Loop
-while not crashed:
-    delta = float(clock.get_time()) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+        clear_color = hsv_shift(clear_color[0], clear_color[1], clear_color[2], delta)    
 
-    clear_color = hsv_shift(clear_color[0], clear_color[1], clear_color[2], delta)    
+        gameDisplay.fill(clear_color) # fill screen with white (clears the screen)
 
-    gameDisplay.fill(clear_color) # fill screen with white (clears the screen)
+        current_scene.logic(delta) # update game logic and data
+        current_scene.draw(gameDisplay) # draw the next frame
+            
+        pygame.display.update() # update the screen (all of the drawing commands like blit are rendered to the screen)
+        clock.tick(60)
+        await asyncio.sleep(0)
 
-    current_scene.logic(delta) # update game logic and data
-    current_scene.draw(gameDisplay) # draw the next frame
-        
-    pygame.display.update() # update the screen (all of the drawing commands like blit are rendered to the screen)
-    clock.tick(60)
+    pygame.quit()
 
-pygame.quit()
-quit()
+if __name__ == "__main__":
+    asyncio.run(main())
